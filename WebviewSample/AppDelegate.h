05abdc79ef0a57f927d61e9e2f0cb190ea4df068
stevernguyen <quynhnv@Quynhs-MacBook-Pro.local>
2015-01-12 22:59:44 +0700

    Initial Commit

---
+++ WebviewSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebviewSample
+//
+//  Created by Quynh Nguyen Van on 1/12/15.
+//  Copyright (c) 2015 Quynh Nguyen Van. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
