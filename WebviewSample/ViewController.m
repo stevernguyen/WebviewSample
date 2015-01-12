3383a14413fa11bb8147ee1a04fb0d24502789e6
stevernguyen <quynhnv@Quynhs-MacBook-Pro.local>
2015-01-13 00:04:29 +0700

    UIWebview sample
    detect content url from uiwebview

--- WebviewSample/ViewController.m
+++ WebviewSample/ViewController.m
@@ -7,9 +7,18 @@
 //
 
 #import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+#import <AVFoundation/AVFoundation.h>
+
+#define NCT     @"http://www.nhaccuatui.com/video.html"
+#define mp3zing @"http://mp3.zing.vn/the-loai-video/Viet-Nam/IWZ9Z08I.html"
+#define youtube @"https://www.youtube.com/watch?v=JpiBjztPf1k"
 
 @interface ViewController ()
-
+{
+    NSString * currentURL;
+}
+@property (weak, nonatomic) IBOutlet UIWebView *webview;
 @end
 
 @implementation ViewController
@@ -17,11 +26,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self initNotificationObservers];
+    
+    NSURL *url = [NSURL URLWithString:mp3zing];
+    [self.webview loadRequest:[[NSURLRequest alloc] initWithURL:url]];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)playerItemBecameCurrent:(NSNotification*)notification {
+    AVPlayerItem *playerItem = [notification object];
+    if(playerItem == nil) return;
+    
+    // Break down the AVPlayerItem to get to the path
+    AVURLAsset *asset = (AVURLAsset*)[playerItem asset];
+    NSURL *url = [asset URL];
+    NSString *path = [url absoluteString];
+    
+    NSLog(@"\n");
+    NSLog(@"\n\n==> CONTENT URL: %@\n\n",path);
+    NSLog(@"\n");
+}
+
+- (void)initNotificationObservers
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(playerItemBecameCurrent:)
+                                                 name:@"AVPlayerItemBecameCurrentNotification"
+                                               object:nil];
+    
 }
 
 @end
